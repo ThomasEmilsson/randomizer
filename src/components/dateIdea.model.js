00a29ad25467d25d4f08e1ccdbc051805a89aba1
ThomasEmilsson <thomasemilsson10@gmail.com>
2020-06-30 22:33:52 +0200

    Add dateIdea model; Add mongoose

---
+++ src/components/dateIdea.model.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose'
+
+const dateIdeaSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 60,
+  },
+  location: {
+    type: String,
+    required: false,
+    trim: true,
+    maxLength: 60,
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+    maxLength: 200,
+  },
+  price: {
+    type: String,
+    enum: ['$', '$$', '$$$'],
+    default: '$',
+  },
+  topics: {
+    type: [String],
+    required: false,
+  },
+  type: {
+    type: [String],
+    enum: [
+      'Romantic',
+      'Casual',
+      'Fun',
+      'Cozy',
+      'Relaxing',
+      'Outdoors',
+      'Adventure',
+      'New',
+    ],
+  },
+  //   created_by: {
+  //     type: mongoose.SchemaTypes.ObjectId,
+  //     ref: 'user',
+  //     required: true,
+})
+
+export const DateIdea = mongoose.model('dateIdea', dateIdeaSchema)
