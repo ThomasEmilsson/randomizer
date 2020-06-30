e7534caff8feb74911444bd6970b26a3fe535e87
ThomasEmilsson <thomasemilsson10@gmail.com>
2020-06-30 23:44:11 +0200

    Connect to Database

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import keys from './config/keys.js'
 
 const app = express()
 const port = 3000
+// app.use(express.json())
+
+const db = keys.mongoURI
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connected to Database'))
+  .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Randomize Me')
+})
+
+app.post('/date', function (req, res) {
+  res.send('Post Request')
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
