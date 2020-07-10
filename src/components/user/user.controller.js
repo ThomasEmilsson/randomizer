0c8cc7bf043a6e81370b7b61d4c4e5f25d6dcdc5
ThomasEmilsson <thomasemilsson10@gmail.com>
2020-07-11 00:04:18 +0200

    Add acceptRequest method

--- src/components/user/user.controller.js
+++ src/components/user/user.controller.js
@@ -1,6 +1,7 @@
 import { User, Partner } from '../user/user.model.js'
 import { DateIdea } from '../dateIdea/dateIdea.model.js'
 import { keys } from '../../config/keys.js'
+import { log } from 'console'
 
 const getCurrentUser = (req, res) => {
   if (req.user && req.session.user) res.status(200).send(req.user)
@@ -36,7 +37,7 @@ const connectRequest = async (req, res) => {
     const partner = await User.findOne({ email: req.query.email })
     const user = await User.findOne({ email: req.user.email })
 
-    if (!partner) {
+    if (!partner || !user) {
       return res
         .status(400)
         .send({ message: 'No account registered with that email' })
@@ -86,7 +87,38 @@ const connectRequest = async (req, res) => {
 
 const acceptRequest = async (req, res) => {
   try {
-    console.log(req.query.email)
+    const partner = await User.findOne({ email: req.query.email })
+    const user = await User.findOne({ email: req.user.email })
+
+    if (!partner || !user) {
+      return res.status(400).send({ message: 'Could not find partner' })
+    }
+
+    const dataForPartner = new Partner({
+      user: user._id,
+      status: 3,
+    })
+
+    const dataForUser = new Partner({
+      user: partner._id,
+      status: 3,
+    })
+
+    let indexToUpdateForPartner = partner.partners.findIndex(
+      (item) => item.user.toString() == user._id.toString()
+    )
+
+    let indexToUpdateForUser = user.partners.findIndex(
+      (item) => item.user.toString() == partner._id.toString()
+    )
+
+    await partner.partners.set(indexToUpdateForPartner, dataForPartner)
+    await partner.save()
+
+    await user.partners.set(indexToUpdateForUser, dataForUser)
+    await user.save()
+
+    res.status(200).end()
   } catch (err) {
     console.error(err)
     res.status(400).end()
