e5e3f86b5c222eaeeb0323030e2df36c57bb5ef6
ThomasEmilsson <thomasemilsson10@gmail.com>
2020-07-16 19:38:41 +0200

    Update theme usage

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './app.scss'
 import '../themes.scss'
+import Button from '../button/button.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
+        <Button theme="default" text="Change To Dark Theme"></Button>
       </div>
     )
   }
--- client/src/components/button/button.js
+++ client/src/components/button/button.js
@@ -2,9 +2,15 @@ import React from 'react'
 import './button.scss'
 
 const Button = (props) => {
+  const { theme } = props
+  function changeToTheme(e) {
+    e.preventDefault()
+  }
   return (
     <div>
-      <button> {props.text} </button>
+      <button onClick={changeToTheme} className={props.theme}>
+        {props.text}
+      </button>
     </div>
   )
 }
